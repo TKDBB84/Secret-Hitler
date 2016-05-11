6b98e907f80218179b5d9f30786745f7de64f10a
Jeffrey Campbell <tkdbb84@gmail.com>
2016-05-10 22:34:00 -0400

    Applied fixes from StyleCI

--- Game/Deck.php
+++ Game/Deck.php
@@ -3,12 +3,9 @@
  * Created by PhpStorm.
  * User: Jeffrey
  * Date: 2/15/2016
- * Time: 9:04 PM
+ * Time: 9:04 PM.
  */
-
 namespace Game;
-
-use \SplEnum as SplEnum;
 
 class Deck
 {
@@ -17,55 +14,63 @@ class Deck
     /** @var Card[] */
     private $discardPile = [];
 
-    public function __construct($numberPlayers = 5) {
-        for($i = 0 ; $i < 6 ; $i++) {
+    public function __construct($numberPlayers = 5)
+    {
+        for ($i = 0; $i < 6; $i++) {
             $this->drawPile[] = new Card(Faction::Liberal);
         }
-        for ($i = 0 ; $i < 11 ; $i++) {
+        for ($i = 0; $i < 11; $i++) {
             $this->drawPile[] = new Card(Faction::Fascist);
         }
         $this->shuffle();
     }
 
-    private function shuffle(){
-        $this->drawPile = array_merge($this->drawPile,$this->discardPile);
+    private function shuffle()
+    {
+        $this->drawPile = array_merge($this->drawPile, $this->discardPile);
         shuffle($this->drawPile);
     }
 
     /**
      * @param $num
+     *
      * @return Card[]
      */
-    public function draw($num) {
-        if(count($this->drawPile) <= $num) {
+    public function draw($num)
+    {
+        if (count($this->drawPile) <= $num) {
             $this->shuffle();
         }
         $cards = [];
-        while($num--) {
+        while ($num--) {
             $cards[] = array_shift($this->drawPile);
         }
         SecretHitler::sendCardCount();
+
         return $cards;
     }
 
     /**
      * @param $num
+     *
      * @return Card[]
      */
-    public function peak($num) {
+    public function peak($num)
+    {
         $peaks = [];
-        if(count($this->drawPile) <= $num) {
+        if (count($this->drawPile) <= $num) {
             $this->shuffle();
         }
 
-        for($i = 0 ; $i < $num ; $i++) {
+        for ($i = 0; $i < $num; $i++) {
             $peaks[] = $this->drawPile[$i];
         }
 
         return $peaks;
     }
 
-    public function discard(Card $card) {
+    public function discard(Card $card)
+    {
         $this->discardPile[] = $card;
         SecretHitler::sendCardCount();
     }
@@ -73,12 +78,13 @@ class Deck
     /**
      * @return int
      */
-    public function getDrawCount(){
+    public function getDrawCount()
+    {
         return count($this->drawPile);
     }
 
-    public function getDiscardCount(){
+    public function getDiscardCount()
+    {
         return count($this->discardPile);
     }
-
 }
--- server.php
+++ server.php
@@ -1,23 +1,19 @@
 <?php
-	set_time_limit(0);
+    set_time_limit(0);
 
+    require_once __DIR__.'/vendor/autoload.php';
+    require __DIR__.'/Game/SecretHitler.php';
 
-	require_once __DIR__ .'/vendor/autoload.php';
-	require __DIR__.'/Game/SecretHitler.php';
+    use Game\SecretHitler;
+    use Ratchet\Http\HttpServer;
+    use Ratchet\Server\IoServer;
+    use Ratchet\WebSocket\WsServer;
 
-	use Game\SecretHitler;
-	use Game\Player;
-	use Ratchet\Http\HttpServer;
-	use Ratchet\WebSocket\WsServer;
+    $server = IoServer::factory(
+        new HttpServer(
+            new WsServer(
+                new SecretHitler()
+            )
+        ), 9124);
 
-	use Ratchet\Server\IoServer;
-
-	$server = IoServer::factory(
-		new HttpServer(
-			new WsServer(
-				new SecretHitler()
-			)
-		),9124);
-
-	$server->run();
-
+    $server->run();
